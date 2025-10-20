e8f19eb6d888264ad6d21871413328b3f6bbe226
huy.phan <huy.phan@digitalfortress.dev>
2025-10-20 14:17:09 +0700

    fix: wrong path slugname

--- src/resources/organizations/organizations.ts
+++ src/resources/organizations/organizations.ts
@@ -3,6 +3,6 @@ import { APIResource } from '../../resource';
 
 export class Organizations extends APIResource {
     checkSlugName(slugName: string, options?: Core.RequestOptions): Core.APIPromise<void> {
-        return this._client.get(`/organizations/check/${slugName}/`, options);
+        return this._client.get(`/organizations/check/${slugName}`, options);
     }
 }
