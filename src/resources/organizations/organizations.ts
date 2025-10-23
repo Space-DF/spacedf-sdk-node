91b949bb2954db7cd5469659c1c0cf2539b8ce2c
Quynh Nguyen <quynh.nguyen@digitalfortress.dev>
2025-10-24 02:05:13 +0700

    feat: check slug org API update

--- src/resources/organizations/organizations.ts
+++ src/resources/organizations/organizations.ts
@@ -2,7 +2,11 @@ import * as Core from '../../core';
 import { APIResource } from '../../resource';
 
 export class Organizations extends APIResource {
-    checkSlugName(slugName: string, options?: Core.RequestOptions): Core.APIPromise<void> {
+    checkSlugName(slugName: string, options?: Core.RequestOptions): Core.APIPromise<CheckSlugNameResponse> {
         return this._client.get(`/organizations/check/${slugName}`, options);
     }
+}
+
+export interface CheckSlugNameResponse {
+    result: string;
 }
