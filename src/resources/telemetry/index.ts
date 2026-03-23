869e4a927d42e21edc6ccdb74de2925038c3645c
huy.phan <huy.phan@digitalfortress.dev>
2026-03-23 13:37:30 +0700

    SPACEDF-19382: [SpaceDF] - [FE] Integrate get list automation api

--- src/resources/telemetry/index.ts
+++ src/resources/telemetry/index.ts
@@ -2,11 +2,13 @@ import { APIResource } from '@space-df/sdk/resource';
 import { Entities } from './entities';
 import { Alerts } from './alerts';
 import { Geofences } from './geofences';
+import { Automations } from './automations';
 import { Events } from './events';
 
 export class Telemetry extends APIResource {
     entities: Entities = new Entities(this._client);
     alerts: Alerts = new Alerts(this._client);
     geofences: Geofences = new Geofences(this._client);
+    automations: Automations = new Automations(this._client);
     events: Events = new Events(this._client);
 }
